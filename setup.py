07b2fbee566b2cb0f439696e9bb8d3c51bb7b3c4
ME-64 <ME-64>
2020-01-16 17:54:36 +0000

    documentation changes

--- setup.py
+++ setup.py
@@ -1,25 +1,40 @@
 from setuptools import setup
+import pathlib
+
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+
 setup(
-  name = 'pyfixerio'
-  packages = ['pyfixerio']
-  version = '0.0.1'
-  license='MIT'
-  description = 'a python API wrapper for the free fixer.io API'
-  author = 'YOUR NAME'
-  author_email = 'your.email@domain.com'
-  url = 'https://github.com/ME-64/pyfixerio'
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['wrapepr', 'FX', 'foreignexchange']
+  name = 'pyfixerio',
+  packages = ['pyfixerio'],
+  version = '0.0.1',
+  license='MIT',
+  description = 'a python API wrapper for the free fixer.io API',
+  long_description = README,
+  long_description_content_type = 'text/markdown',
+  author = 'ME-64',
+  author_email = 'miloelliott64@gmail.com',
+  url = 'https://github.com/ME-64/pyfixerio',
+  # download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['wrapepr', 'FX', 'foreignexchange'],
+  include_package_data = True,
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'requests'
+          'urllib'
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',     
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3.5',
+    'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+
+
+
+
   ],
 )
