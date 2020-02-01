01c4a943d34e2ccfdbba9453daeb63a1e9080c8d
ME-64 <53708253+ME-64@users.noreply.github.com>
2020-02-01 11:46:38 +0000

    increment version for release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 setup(
   name = 'pyfixerio',
   packages = ['pyfixerio'],
-  version = '0.1.1',
+  version = '0.1.2',
   license='MIT',
   description = 'a python API wrapper for the free fixer.io API',
   long_description = README,
@@ -17,10 +17,9 @@ setup(
   author = 'ME-64',
   author_email = 'miloelliott64@gmail.com',
   url = 'https://github.com/ME-64/pyfixerio',
-  # download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['wrapepr', 'FX', 'foreignexchange'],
   include_package_data = True,
-  install_requires=[            # I get to this in a second
+  install_requires=[           
           'requests',
       ],
   classifiers=[
